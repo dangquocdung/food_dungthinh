383a9b7dfbaa95a9eafe9d11543c9a0b5c310e8c
dangquocdung <dungthinhvn@gmail.com>
2021-01-30 00:07:36 +0700

    asdsad

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -44,10 +44,10 @@ class UploadController extends Controller
         $array = explode('.', $conversion . $filename);
         $extension = strtolower(end($array));
         if (isset($filename)) {
-            return response()->file(storage_path($id . '/' . $conversion . '/' . $filename));
+            return response()->file(storage_path('app/public/' . $id . '/' . $conversion . '/' . $filename));
         } else {
             $filename = $conversion;
-            return response()->file(storage_path($id . '/' . $filename));
+            return response()->file(storage_path('app/public/' . $id . '/' . $filename));
         }
 
     }
